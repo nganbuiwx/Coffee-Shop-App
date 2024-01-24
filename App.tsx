375de973a4a89713699b46d48c556c55fcc0ab15
trinhbaotoan <hu.toan@hitachivantara.com>
2024-01-24 17:25:16 +0700

    24-01-2024

--- App.tsx
+++ App.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import TabNavigator from './src/navigators/TabNavigator';
+import DetailsScreen from './src/screens/DetailsScreen';
+import PaymentScreen from './src/screens/PaymentScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello world 1</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name="Tab"
+          component={TabNavigator}
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+
+        <Stack.Screen
+          name="Details"
+          component={DetailsScreen}
+          options={{animation: 'slide_from_bottom'}}
+        />
+
+        <Stack.Screen
+          name="Payment"
+          component={PaymentScreen}
+          options={{animation: 'slide_from_bottom'}}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'blue',
-  },
-});
 
 export default App;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,60 @@
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {useStore} from '../store/store';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { COLORS } from '../theme/theme';
+
+const getCategoriesFromData = (data: any) => {
+  let temp: any = {};
+
+  for (let i = 0; i < data.length; i++){
+    if (temp[data[i].name] == undefined){
+      temp[data[i].name] = 1;
+    } else {
+      temp[data[i].name]++;
+    }
+  }
+  let categories = Object.keys(temp);
+  categories.unshift('All');
+  return categories;
+};
+
+const getCoffeeList = (category: string, data:any) => {
+  if(category == "All"){
+    return data;
+  }else{
+    let coffeelist = data.filter((item: any) => item.name == category);
+    return coffeelist;
+  }
+}
+
+const HomeScreen = () => {
+  const CoffeeList = useStore((state: any)=> state.CoffeeList);
+  const BeanList = useStore((state: any) => state.BeanList);
+  const [categories, setCategories] = useState(getCategoriesFromData(CoffeeList));
+  const [searchText, setSearchText] = useState(undefined);
+  const [categoryIndex, setCategoryIndex] = useState({
+    index: 0,
+    category: categories[0],
+  });
+  const [sortedCoffee, setSortedCoffee] = useState(
+    getCoffeeList(categoryIndex.category, CoffeeList),
+  );
+  
+  const tabBarHeight = useBottomTabBarHeight();
+
+
+  return <View style={styles.ScreenContainer}>
+    <StatusBar backgroundColor={COLORS.primaryBlackHex}/>
+  </View>
+}
+
+const styles = StyleSheet.create({
+  ScreenContainer: {
+    flex: 1,
+    backgroundColor: COLORS.primaryBlackHex,
+
+  }
+});
+
+export default HomeScreen;
